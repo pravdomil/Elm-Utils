b4f7da417be8caa4af3023c1ea8459e43b8616a2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-26 16:16:49 +0200

    Utils.Update

---
+++ src/Utils/Update.elm
@@ -0,0 +1,15 @@
+module Utils.Update exposing (..)
+
+
+multiple : (msg -> model -> ( model, Cmd msg )) -> List msg -> model -> ( model, Cmd msg )
+multiple updateFn msgs model =
+    msgs
+        |> List.foldl
+            (\msg ( model_, cmds ) ->
+                updateFn msg model_
+                    |> Tuple.mapSecond (\v -> v :: cmds)
+            )
+            ( model
+            , []
+            )
+        |> Tuple.mapSecond Cmd.batch
