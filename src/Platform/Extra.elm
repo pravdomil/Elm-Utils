96f792aefd1c6a5e8949fc9481cb22893aa076f3
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-08-24 18:31:19 +0200

    andThen

--- src/Platform/Extra.elm
+++ src/Platform/Extra.elm
@@ -24,6 +24,17 @@ updateMultiple updateFn msgs model =
         |> Tuple.mapSecond Cmd.batch
 
 
+andThen : (model -> ( model, Cmd msg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
+andThen fn ( model, cmd ) =
+    let
+        ( nextModel, cmd2 ) =
+            fn model
+    in
+    ( nextModel
+    , Cmd.batch [ cmd2, cmd ]
+    )
+
+
 sendMsg : a -> Cmd a
 sendMsg a =
     Task.succeed () |> Task.perform (\_ -> a)
