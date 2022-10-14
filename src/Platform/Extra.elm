bdb6cd0e3ebcda57d0244df3aff99b43a2410850
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-14 19:17:12 +0200

    next

--- src/Platform/Extra.elm
+++ src/Platform/Extra.elm
@@ -1,28 +1,5 @@
 module Platform.Extra exposing (..)
 
-import Task
-
-
-applyInit : ( arg, Cmd msg ) -> ( arg -> model, Cmd msg ) -> ( model, Cmd msg )
-applyInit ( arg, argCmd ) ( toModel, cmd ) =
-    ( toModel arg
-    , Cmd.batch [ argCmd, cmd ]
-    )
-
-
-updateMultiple : (msg -> model -> ( model, Cmd msg )) -> List msg -> model -> ( model, Cmd msg )
-updateMultiple updateFn msgs model =
-    msgs
-        |> List.foldl
-            (\msg ( model_, cmds ) ->
-                updateFn msg model_
-                    |> Tuple.mapSecond (\v -> v :: cmds)
-            )
-            ( model
-            , []
-            )
-        |> Tuple.mapSecond Cmd.batch
-
 
 andThen : (model -> ( model, Cmd msg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 andThen fn ( model, cmd ) =
@@ -33,8 +10,3 @@ andThen fn ( model, cmd ) =
     ( nextModel
     , Cmd.batch [ cmd2, cmd ]
     )
-
-
-sendMsg : a -> Cmd a
-sendMsg a =
-    Task.succeed () |> Task.perform (\_ -> a)
