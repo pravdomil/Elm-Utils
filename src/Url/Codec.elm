b5ff3b99622f617d684749aa99d3ad854e671c6f
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-21 14:56:39 +0200

    next

---
+++ src/Url/Codec.elm
@@ -0,0 +1,19 @@
+module Url.Codec exposing (..)
+
+import Codec
+import Url
+
+
+codec : Codec.Codec Url.Url
+codec =
+    Codec.string
+        |> Codec.andThen
+            Url.toString
+            (\x ->
+                case Url.fromString x of
+                    Just x2 ->
+                        Codec.succeed x2
+
+                    Nothing ->
+                        Codec.fail "Cannot decode URL."
+            )
