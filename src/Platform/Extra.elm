0456910dc0f68152bd0dfad42eee39a911773bda
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-11-30 21:25:10 +0100

    next

--- src/Platform/Extra.elm
+++ src/Platform/Extra.elm
@@ -4,11 +4,11 @@ module Platform.Extra exposing (..)
 andThen : (model -> ( model, Cmd msg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 andThen fn ( model, cmd ) =
     let
-        ( nextModel, cmd2 ) =
+        ( nextModel, nextCmd ) =
             fn model
     in
     ( nextModel
-    , Cmd.batch [ cmd2, cmd ]
+    , Cmd.batch [ nextCmd, cmd ]
     )
 
 
