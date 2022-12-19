b556abe4d1d7b9c69ab41dda2d58aec7fa756e1a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-12-19 19:23:19 +0100

    next

--- src/Browser/Router.elm
+++ src/Browser/Router.elm
@@ -1,4 +1,4 @@
-module Browser.Router exposing (Router, init, requestUrl, sameDirectory, samePath, update)
+module Browser.Router exposing (Router, init, requestUrl, update, urlToDirectoryBaseUrl, urlToFileBaseUrl)
 
 import Browser
 import Browser.Navigation
@@ -12,17 +12,20 @@ type alias Router a =
     }
 
 
-init : (Url.Url -> a) -> Url.Url -> Browser.Navigation.Key -> Router a
-init toState url key =
-    Router key url (toState url)
+init : (Url.Url -> Url.Url) -> (Url.Url -> a) -> Url.Url -> Browser.Navigation.Key -> Router a
+init toBaseUrl toState url key =
+    Router
+        key
+        (toBaseUrl url)
+        (toState url)
 
 
-requestUrl : (Url.Url -> Url.Url -> Bool) -> Browser.UrlRequest -> { model | router : Router a } -> ( { model | router : Router a }, Cmd msg )
-requestUrl isInternalUrl req model =
+requestUrl : (Url.Url -> Url.Url) -> Browser.UrlRequest -> { model | router : Router a } -> ( { model | router : Router a }, Cmd msg )
+requestUrl toBaseUrl req model =
     ( model
     , case req of
         Browser.Internal url ->
-            if isInternalUrl model.router.baseUrl url then
+            if toBaseUrl url == model.router.baseUrl then
                 Browser.Navigation.pushUrl model.router.key (Url.toString url)
 
             else
@@ -46,20 +49,21 @@ update toState url model =
 --
 
 
-samePath : Url.Url -> Url.Url -> Bool
-samePath a b =
-    (a.protocol == b.protocol)
-        && (a.host == b.host)
-        && (a.port_ == b.port_)
-        && (a.path == b.path)
+urlToFileBaseUrl : Url.Url -> Url.Url
+urlToFileBaseUrl a =
+    { a
+        | query = Nothing
+        , fragment = Nothing
+    }
 
 
-sameDirectory : Url.Url -> Url.Url -> Bool
-sameDirectory a b =
-    (a.protocol == b.protocol)
-        && (a.host == b.host)
-        && (a.port_ == b.port_)
-        && (dropAfter "/" a.path == dropAfter "/" b.path)
+urlToDirectoryBaseUrl : Url.Url -> Url.Url
+urlToDirectoryBaseUrl a =
+    { a
+        | path = dropAfter "/" a.path
+        , query = Nothing
+        , fragment = Nothing
+    }
 
 
 
