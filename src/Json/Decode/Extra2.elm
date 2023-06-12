0d7e52ef52e0c06bf2513792bc185cf477a85eb2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-06-12 22:31:01 +0200

    next

--- src/Json/Decode/Extra2.elm
+++ src/Json/Decode/Extra2.elm
@@ -1,8 +1,26 @@
 module Json.Decode.Extra2 exposing (..)
 
 import Json.Decode
+import Json.Decode.Extra2
 
 
 apply : Json.Decode.Decoder a -> Json.Decode.Decoder (a -> b) -> Json.Decode.Decoder b
 apply decoder a =
     Json.Decode.map2 (\fn x -> fn x) a decoder
+
+
+maybeField : String -> Json.Decode.Decoder a -> Json.Decode.Decoder (Maybe a)
+maybeField a decoder =
+    Json.Decode.oneOf
+        [ Json.Decode.field a (Json.Decode.map Just Json.Decode.value)
+        , Json.Decode.succeed Nothing
+        ]
+        |> Json.Decode.andThen
+            (\x ->
+                case x of
+                    Just _ ->
+                        Json.Decode.field a (Json.Decode.map Just decoder)
+
+                    Nothing ->
+                        Json.Decode.succeed Nothing
+            )
