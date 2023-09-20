667044e9d12693926fc57e35c0197cd153b93375
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-20 19:59:10 +0200

    next

--- src/Http/Resolver.elm
+++ src/Http/Resolver.elm
@@ -6,20 +6,6 @@ import Http
 import Json.Decode as Decode
 
 
-json : Decode.Decoder a -> Http.Resolver Http.Error a
-json a =
-    customString (\x -> Result.mapError Decode.errorToString (Decode.decodeString a x))
-
-
-codec : Codec.Codec a -> Http.Resolver Http.Error a
-codec a =
-    customString (\x -> Result.mapError Decode.errorToString (Codec.decodeString a x))
-
-
-
---
-
-
 customString : (String -> Result String a) -> Http.Resolver Http.Error a
 customString a =
     Http.stringResolver (helper a)
@@ -28,6 +14,16 @@ customString a =
 customBytes : (Bytes.Bytes -> Result String a) -> Http.Resolver Http.Error a
 customBytes a =
     Http.bytesResolver (helper a)
+
+
+json : Decode.Decoder a -> Http.Resolver Http.Error a
+json a =
+    customString (\x -> Result.mapError Decode.errorToString (Decode.decodeString a x))
+
+
+codec : Codec.Codec a -> Http.Resolver Http.Error a
+codec a =
+    customString (\x -> Result.mapError Decode.errorToString (Codec.decodeString a x))
 
 
 
