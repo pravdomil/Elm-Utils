2f3cecd2f7816739087b99d5cd4efcd72123d9c3
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-20 20:51:10 +0200

    next

--- src/Http/Resolver.elm
+++ src/Http/Resolver.elm
@@ -2,8 +2,9 @@ module Http.Resolver exposing (..)
 
 import Bytes
 import Codec
+import Dict
 import Http
-import Json.Decode as Decode
+import Json.Decode
 
 
 string : (String -> Result String a) -> Http.Resolver Http.Error a
@@ -17,8 +18,28 @@ json a =
 
 
 codec : Codec.Codec a -> Http.Resolver Http.Error a
-codec a =
-    string (\x -> Result.mapError Decode.errorToString (Codec.decodeString a x))
+codec codec_ =
+    let
+        decode : Http.Metadata -> String -> Result Http.Error a
+        decode meta a =
+            case Dict.get "content-type" meta.headers of
+                Just b ->
+                    case b of
+                        "application/json" ->
+                            case Codec.decodeString codec_ a of
+                                Ok c ->
+                                    Ok c
+
+                                Err c ->
+                                    Err (Http.BadBody (Json.Decode.errorToString c))
+
+                        _ ->
+                            Err (Http.BadBody ("Bad content type " ++ b ++ ", status is " ++ String.fromInt meta.statusCode ++ "."))
+
+                Nothing ->
+                    Err (Http.BadBody "No content type.")
+    in
+    Http.stringResolver (helper2 decode)
 
 
 bytes : (Bytes.Bytes -> Result String a) -> Http.Resolver Http.Error a
