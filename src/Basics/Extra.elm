38b85038e44b785962b13a36274aba668954e37b
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-30 21:13:49 +0200

    next

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -8,3 +8,10 @@ module Basics.Extra exposing (..)
 referenceEqual : a -> a -> Bool
 referenceEqual a b =
     False
+
+
+{-| Patched by Elm FFI.
+-}
+memorize : (a -> b) -> (a -> b)
+memorize fn =
+    \x -> fn x
