ed78e817da9109e665feacfa445b94ac1f74ffc2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-30 15:16:06 +0200

    next

--- src/String/Extra2.elm
+++ src/String/Extra2.elm
@@ -1,1 +1,15 @@
 module String.Extra2 exposing (..)
+
+
+splitByLast : String -> String -> Maybe ( String, String, String )
+splitByLast needle a =
+    case List.head (List.reverse (String.indexes needle a)) of
+        Just b ->
+            Just
+                ( String.left b a
+                , needle
+                , String.dropLeft (b + 1) a
+                )
+
+        Nothing ->
+            Nothing
